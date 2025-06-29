feb70ae4bbb8a158bc23bef271340115ce6c94c5
shef <sgo8308@gmail.com>
2025-06-29 23:39:16 +0900

    MethodReferenceFinder로 메소드에서 사용된 클래스 찾는 메소드 이동

--- src/main/kotlin/org/example/emm/MethodReferenceFinder.kt
+++ src/main/kotlin/org/example/emm/MethodReferenceFinder.kt
@@ -4,6 +4,7 @@ import com.intellij.openapi.project.Project
 import com.intellij.psi.*
 import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.search.searches.ReferencesSearch
+import com.intellij.psi.util.PsiUtil
 import com.intellij.psi.util.parentOfType
 
 /**
@@ -66,5 +67,67 @@ class MethodReferenceFinder(private val project: Project) {
         }
 
         return externalClassesToInject
+    }
+
+    /**
+     * 메소드에서 사용된 클래스 수집
+     */
+     fun collectUsedClasses(method: PsiMethod): Set<PsiClass> {
+        val usedClasses = mutableSetOf<PsiClass>()
+
+        // 반환 타입
+        method.returnType?.let { returnType ->
+            if (returnType is PsiClassType) {
+                PsiUtil.resolveClassInType(returnType)?.let { usedClasses.add(it) }
+                returnType.parameters.mapNotNull { parameterType ->
+                    PsiUtil.resolveClassInType(parameterType)
+                }.forEach { usedClasses.add(it) }
+            }
+        }
+
+        // 매개변수 타입
+        method.parameterList.parameters.forEach { param ->
+            val paramType = param.type
+            if (paramType is PsiClassType) {
+                PsiUtil.resolveClassInType(paramType)?.let { usedClasses.add(it) }
+                paramType.parameters.mapNotNull { parameterType ->
+                    PsiUtil.resolveClassInType(parameterType)
+                }.forEach { usedClasses.add(it) }
+            }
+        }
+        // throws 예외 타입
+        method.throwsList.referencedTypes.forEach { type ->
+            type.resolve()?.let { usedClasses.add(it) }
+        }
+
+        // 메소드 내부에서 사용된 클래스
+        method.body?.let { body ->
+            body.accept(object : JavaRecursiveElementVisitor() {
+                override fun visitReferenceExpression(expression: PsiReferenceExpression) {
+                    super.visitReferenceExpression(expression)
+                    expression.resolve()?.let { resolved ->
+                        if (resolved is PsiClass) {
+                            usedClasses.add(resolved)
+                        }
+                    }
+                }
+
+                override fun visitTypeElement(typeElement: PsiTypeElement) {
+                    super.visitTypeElement(typeElement)
+                    PsiUtil.resolveClassInType(typeElement.type)?.let { usedClasses.add(it) }
+                }
+
+                override fun visitNewExpression(expression: PsiNewExpression) {
+                    super.visitNewExpression(expression)
+                    expression.classReference?.resolve()?.let { resolved ->
+                        if (resolved is PsiClass) {
+                            usedClasses.add(resolved)
+                        }
+                    }
+                }
+            })
+        }
+
+        return usedClasses
     }
 }
--- src/main/kotlin/org/example/emm/MyRefactorAction.kt
+++ src/main/kotlin/org/example/emm/MyRefactorAction.kt
@@ -101,7 +101,7 @@ class MyRefactorAction : AnAction() {
                 }
 
             // 이동 대상 메소드에 필요한 import 추가
-            val usedClasses = collectUsedClasses(originalMethod)
+            val usedClasses = methodReferenceFinder.collectUsedClasses(originalMethod)
             addImportsToTargetClass(targetClass, usedClasses)
 
             // 이동 대상 메소드를 대상 클래스에 복사
@@ -174,68 +174,6 @@ class MyRefactorAction : AnAction() {
             "메서드 이동 완료",
             Messages.getInformationIcon()
         )
-    }
-
-    /**
-     * 메소드에서 사용된 클래스 수집
-     */
-    private fun collectUsedClasses(method: PsiMethod): Set<PsiClass> {
-        val usedClasses = mutableSetOf<PsiClass>()
-
-        // 반환 타입
-        method.returnType?.let { returnType ->
-            if (returnType is PsiClassType) {
-                PsiUtil.resolveClassInType(returnType)?.let { usedClasses.add(it) }
-                returnType.parameters.mapNotNull { parameterType ->
-                    PsiUtil.resolveClassInType(parameterType)
-                }.forEach { usedClasses.add(it) }
-            }
-        }
-
-        // 매개변수 타입
-        method.parameterList.parameters.forEach { param ->
-            val paramType = param.type
-            if (paramType is PsiClassType) {
-                PsiUtil.resolveClassInType(paramType)?.let { usedClasses.add(it) }
-                paramType.parameters.mapNotNull { parameterType ->
-                    PsiUtil.resolveClassInType(parameterType)
-                }.forEach { usedClasses.add(it) }
-            }
-        }
-        // throws 예외 타입
-        method.throwsList.referencedTypes.forEach { type ->
-            type.resolve()?.let { usedClasses.add(it) }
-        }
-
-        // 메소드 내부에서 사용된 클래스
-        method.body?.let { body ->
-            body.accept(object : JavaRecursiveElementVisitor() {
-                override fun visitReferenceExpression(expression: PsiReferenceExpression) {
-                    super.visitReferenceExpression(expression)
-                    expression.resolve()?.let { resolved ->
-                        if (resolved is PsiClass) {
-                            usedClasses.add(resolved)
-                        }
-                    }
-                }
-
-                override fun visitTypeElement(typeElement: PsiTypeElement) {
-                    super.visitTypeElement(typeElement)
-                    PsiUtil.resolveClassInType(typeElement.type)?.let { usedClasses.add(it) }
-                }
-
-                override fun visitNewExpression(expression: PsiNewExpression) {
-                    super.visitNewExpression(expression)
-                    expression.classReference?.resolve()?.let { resolved ->
-                        if (resolved is PsiClass) {
-                            usedClasses.add(resolved)
-                        }
-                    }
-                }
-            })
-        }
-
-        return usedClasses
     }
 
     /**
